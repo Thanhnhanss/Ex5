a5ba552b6d9e8112c65c4bb729556ecd63b87954
Thanhnhanss <@gmail.com>
2019-10-15 00:16:34 +0700

    GamePickBalls

--- PickBalls/PickBalls/Program.cs
+++ PickBalls/PickBalls/Program.cs
@@ -10,34 +10,95 @@ namespace PickBalls
     {
         static void Main(string[] args)
         {
-            int[] game = new int[] {0,3,4,6};
-            PrintGame(game);
+            int[] game = new int[] { 0, 3, 4, 6 };
+
             while (true)
             {
-                try
+                HumanMove(game);
+                PrintGame(game);
+                if (Has0Group(game))
                 {
-            HumanMove(game);
-            PrintGame(game);
-            if ( Has0Group(game))
-            {
-                Console.WriteLine("You lose!");
-                break;
-            }
-            Console.WriteLine("Computer is thinking...");
-            ComputerMove(game);
-            PrintGame(game);
-            if (Has0Group(game))
-            {
-                Console.WriteLine("You win!!");
-                break;
+                    Console.WriteLine("You lose!");
+                    break;
+                }
+                Console.WriteLine("Computer is thinking...");
+                ComputerMove(game);
+                PrintGame(game);
+                if (Has0Group(game))
+                {
+                    Console.WriteLine("You win!!");
+                    break;
+                }
             }
         }
-         catch(Exception) 
+        public static void HumanMove(int[] game)
+        {
+            Console.WriteLine("Your turn: ");
+            Console.Write("Which group do you choose?  ");
+            int group = int.Parse(Console.ReadLine());
+            Console.Write("How many balls will you pick?  ");
+            int balls = int.Parse(Console.ReadLine());
+            PickBalls(game, group, balls);
+        }
+
+        public static void ComputerMove(int[] game)
+        {
+            if (Has1Group(game))
             {
-                Console.WriteLine();
+                int g = 0;
+                Get1Group(game, out g);
+                if (game[g] > 1)
+                    PickBalls(game, g, game[g] - 1);
+                else
+                    PickBalls(game, g, 1);
+            }
+            else if (Has2Group(game))
+            {
+                int a = 0, b = 0;
+                Get2Group(game, out a, out b);
+                if (game[a] != game[b])
+                {
+                    if (game[a] == 1)
+                        PickBalls(game, b, game[b]);
+                    else if (game[b] == 1)
+                        PickBalls(game, a, game[a]);
+                    else if (game[a] > game[b])
+                        PickBalls(game, a, game[a] - game[b]);
+                    else
+                        PickBalls(game, b, game[b - game[a]]);
+                }
+                else
+                    PickBalls(game, a, 1);
+            }
+            else
+            {
+                Random rand = new Random();
+                int group = rand.Next(1, 3);
+                int balls = rand.Next(1, game[group]);
+                PickBalls(game, group, balls);
             }
         }
-    }
+
+        private static void Get1Group(int[] game, out int g)
+        {
+            throw new NotImplementedException();
+        }
+        public static void PrintGame(int[] game)
+        {
+            Console.Write("Group 1:");
+            for (int i = 0; i < game[1]; i++)
+                Console.Write("o");
+            Console.WriteLine();
+            Console.Write("Group 2:");
+            for (int i = 0; i < game[2]; i++)
+                Console.Write("o");
+            Console.WriteLine();
+            Console.Write("Group 3:");
+            for (int i = 0; i < game[3]; i++)
+                Console.Write("o");
+            Console.WriteLine();
+            Console.WriteLine("\n");
+        }
 
         public static void PickBalls(int[] game, int group, int balls)
         {
@@ -60,7 +121,7 @@ namespace PickBalls
             return false;
         }
 
-        public static void Get1Groups(int[] game, out int a, out int b)
+        public static void Get1Group(int[] game, out int a, out int b)
         {
             a = -1;
             b = -1;
@@ -92,7 +153,7 @@ namespace PickBalls
             return false;
         }
         
-        public static void Get2Groups(int[] game, out int a, out int b)
+        public static void Get2Group(int[] game, out int a, out int b)
         {
             a = b = 0;
             if (game[1] > 0 && game[2] > 0 && game[3] == 0)
@@ -110,62 +171,8 @@ namespace PickBalls
                 b = 3;
             }
         }
-        
-        public static void PrintGame(int[] game)
-        {
-            Console.WriteLine("Group 1: ");
-        }
-
-        public static void HumanMove(int[] game)
-        {
-            Console.WriteLine("Which group do you choose? ");
-            int group = int.Parse(Console.ReadLine());
-            Console.Write("How many balls will you picks? ");
-            int balls = int.Parse(Console.ReadLine());
-            PickBalls(game, group, balls);
-        }
-
-        public static void ComputerMove(int[] game)
-        {
-            if (Has1Group(game))
-            {
-                int g = 0;
-                Get1Groups(game, out g);
-                if (game[g] > 1)
-                    PickBalls(game, g, game[g] - 1);
-                else
-                    PickBalls(game, g, 1);
-            }
-            if (Has2Group(game))
-            {
-                int a = 0, b = 0;
-                Get2Groups(game, out a, out b);
-                if (game[a] != game[b])
-                {
-                    if (game[a] == 1)
-                        PickBalls(game, b, game[b]);
-                    else if (game[b] == 1)
-                        PickBalls(game, a, game[a]);
-                    else if (game[a] > game[b])
-                        PickBalls(game, a, game[a] - game[b]);
-                    else
-                        PickBalls(game, b, game[b - game[a]]);
-                }
-                else
-                    PickBalls(game, a, 1);
-            }
-            else
-            {
-                Random rand = new Random();
-                int group = rand.Next(1, 3);
-                int balls = rand.Next(1, game[group]);
-                PickBalls(game, group, balls);
-                Console.WriteLine("Computer pick {0} balls from group {1}", balls, group + 1);
-            }
-        }
+     
     }
 }
-
-
 
 
